4d4ff81dbb7969815258fafbc3fe4f319f38e68b
prasanv <prasan.venkatb@gmail.com>
2025-09-23 22:18:17 -0700

    add more demo project

---
+++ 2_springJBDC/src/main/java/com/example/springJBDC/SpringJbdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springJBDC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJbdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJbdcApplication.class, args);
+	}
+
+}
