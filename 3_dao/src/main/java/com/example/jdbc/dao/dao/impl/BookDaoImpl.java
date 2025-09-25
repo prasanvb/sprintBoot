6e2c39f7ed47dd6f7071697360804b5b848632f6
Prasan Bala <34255378+prasanvb@users.noreply.github.com>
2025-09-25 15:51:02 -0700

    Create BookDaoImpl.java

---
+++ 3_dao/src/main/java/com/example/jdbc/dao/dao/impl/BookDaoImpl.java
@@ -0,0 +1,12 @@
+package com.example.jdbc.dao.dao.impl;
+
+import com.example.jdbc.dao.dao.BookDao;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class BookDaoImpl implements BookDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
