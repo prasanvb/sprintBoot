851f857f50c136b5737d5cd0c309b0fb01edc717
prasanv <prasan.venkatb@gmail.com>
2025-09-25 07:55:25 -0700

    added new sql schema file

--- 3_dao/src/main/java/com/example/jdbc/dao/DaoApplication.java
+++ 3_dao/src/main/java/com/example/jdbc/dao/DaoApplication.java
@@ -1,13 +1,34 @@
 package com.example.jdbc.dao;
 
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class DaoApplication {
+@Log
+public class DaoApplication  implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    private final DataSource dataSource;
+
+    public DaoApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    public static void main(String[] args) {
 		SpringApplication.run(DaoApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) {
+        log.info("Postgres dataSource: "+ dataSource.toString());
+        final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+        restTemplate.execute("select 1");
+    }
+    
 }
