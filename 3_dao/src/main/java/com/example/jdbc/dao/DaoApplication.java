d698880371191a42788647db231c3391fead794a
prasanv <prasan.venkatb@gmail.com>
2025-09-25 21:35:19 -0700

    fix minor issues

--- 3_dao/src/main/java/com/example/jdbc/dao/DaoApplication.java
+++ 3_dao/src/main/java/com/example/jdbc/dao/DaoApplication.java
@@ -1,7 +1,7 @@
 package com.example.jdbc.dao;
 
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
